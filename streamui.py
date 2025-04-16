940eb0d9516059778c56eec4c51540ca6d127f22
Juma <117809851+Spoiler-xl@users.noreply.github.com>
2025-04-16 11:04:43 +0300

    Update streamui.py

--- streamui.py
+++ streamui.py
@@ -1,19 +1,20 @@
 import streamlit as st
 import urllib.request
+import gzip
 import pickle
-import numpy as np  # You missed importing numpy
+import numpy as np  
 import os
 
 # --- Model Download URL ---
-MODEL_URL = "https://github.com/Spoiler-xl/climate-model/releases/download/V1.0/rf.pkl"
-MODEL_FILE = "rf.pkl"
+MODEL_URL = "https://github.com/Spoiler-xl/climate-model/releases/download/V1.0/rf.pkl.gz"
+MODEL_FILE = "rf.pkl.gz"
 
 # --- Load Model Function ---
 @st.cache_data
 def load_model():
     if not os.path.exists(MODEL_FILE):
         urllib.request.urlretrieve(MODEL_URL, MODEL_FILE)
-    with open(MODEL_FILE, "rb") as f:
+    with gzip.open(MODEL_FILE, "rb") as f:
         model = pickle.load(f)
     return model
 
